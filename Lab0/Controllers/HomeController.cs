e84ac947de84f7a3069f0707a8f182d13da2543e
piotr.bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-10-07 16:26:56 +0200

    lab01 - Calculator action

--- Lab0/Controllers/HomeController.cs
+++ Lab0/Controllers/HomeController.cs
@@ -22,6 +22,38 @@ public class HomeController : Controller
         return View();
     }
 
+    
+    public IActionResult Calculator(double? a, double? b, [FromQuery(Name = "operator")]string op)
+    {
+        
+        // Definiujemy działanie dodawnaia a oraz b
+        string result = "";
+        
+        // Sprawdzamy czy a oraz b sa null
+        if (a is null || b is null)
+        {
+            return View("Calculator", "Brak parametru a lub b");
+        }
+        
+        // w op może wystapić: add, sub, mul, div
+        switch (op)
+        {
+            case "add": result = $"{a} + {b} = {a + b}";
+                break;
+            case "sub": result = $"{a} - {b} = {a - b}";
+                break;
+            case "mul": result = $"{a} * {b} = {a * b}";
+                break;
+            case "div": result = $"{a} / {b} = {a / b}";
+                break;
+            default:
+                result = "Nieznany operator!";
+                break;
+                
+        }
+        return View("Calculator",result);
+    }
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
