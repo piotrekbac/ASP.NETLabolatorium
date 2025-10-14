60312560476c17e351ec041d3c54f07c5f2d5af5
piotr.bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-10-14 16:06:10 +0200

    CalculatorControler.cs

---
+++ Lab0/Controllers/CalculatorControler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab0.Controllers;
+
+public class CalculatorController : Controller
+{
+    // GET
+    public IActionResult Form()
+    {
+        return View();
+    }
+    
+    public IActionResult Result(double? a, double? b, [FromQuery(Name = "op")]string op)
+    {
+        
+        // Definiujemy działanie dodawnaia a oraz b
+        string result = "";
+        
+        // Sprawdzamy czy a oraz b sa null
+        if (a is null || b is null)
+        {
+            return View("Calculator", "Brak parametru a lub b");
+        }
+        
+        // w op może wystapić: add, sub, mul, div
+        switch (op)
+        {
+            case "add": result = $"{a} + {b} = {a + b}";
+                break;
+            case "sub": result = $"{a} - {b} = {a - b}";
+                break;
+            case "mul": result = $"{a} * {b} = {a * b}";
+                break;
+            case "div": result = $"{a} / {b} = {a / b}";
+                break;
+            default:
+                result = "Nieznany operator!";
+                break;
+                
+        }
+        ViewBag.Result = result;
+        return View();
+    }
+}
