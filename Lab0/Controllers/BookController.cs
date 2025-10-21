706052e0def928b301b48d76a558a73148b8f2fe
piotr.bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-10-21 16:44:07 +0200

    end of lab03

---
+++ Lab0/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using Lab0.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab0.Controllers;
+
+public class BookController : Controller
+{
+    private static Dictionary<int, Book> _books = new();
+
+    private static int i = 0; 
+    
+    // GET
+    public IActionResult Index()
+    {
+        return View(_books.Values.ToList());
+    }
+
+    
+    [HttpGet]       // Wyświetlenie formularza dodania obiektu 
+    public IActionResult Create()
+    {
+        return View();  
+    }
+    
+    [HttpPost]       // Odbiór danych obiektu i zapisanie ich do bazy 
+    public IActionResult Create(Book model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        // zapisanie obiketu
+        model.Id = ++i;                     // nadajemy modelowi odpowiednie id (zwiększając je o 1)
+        _books.Add(model.Id, model);        // następnie odpowiednio dodajemy te modele i id. 
+        return RedirectToAction("Index");   // przejdź do listy obiketów
+    }
+}
---
+++ Lab0/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab0.Models;
+
+public class Book
+{
+    [HiddenInput]
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(maximumLength: 100, MinimumLength = 2)]
+    public string? Title { get; set; }
+    
+    [StringLength(maximumLength: 350, MinimumLength = 2)]
+    public string? Author { get; set; } 
+    
+    [Range(minimum:1, maximum: 10000)]
+    public int PageNumber { get; set; }
+    
+    [Required]
+    [StringLength(maximumLength: 350, MinimumLength = 5)]
+    public string? ISBN { get; set; }
+    
+    [Range(minimum:-350, maximum: 3500)]
+    public int YearOfPublishing { get; set; }
+    
+    [StringLength(maximumLength: 80, MinimumLength = 1)]
+    public string? Publisher { get; set; }
+    
+}
